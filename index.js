385c53fd6624f04663ab68db0c58c4f29d1cc149
JoelKM <joelkasmor16@gmail.com>
2023-03-26 03:53:58 +0200

    Remove log

--- index.js
+++ index.js
@@ -20,17 +20,12 @@ console.log("DePerrito aktivo klk");
 client.on("messageCreate", async (message) => {
     const command = message.content.split(" ");
 
-    for (let index = 0; index < command.length; index++) {
-        console.log(command[index]);
-    }
-
     if(command[0]=='$pls') {
         switch (command[1]) {
             case 'help':
                 message.channel.send("///LISTA DE COMANDOS///");
             case 'perrea':
                 message.channel.send("*perrea guarro e intenso*");
-                console.log("funciona");
                 break;
             default:
                 message.channel.send("Invalid command");
