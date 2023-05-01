a2fdc50714d82387fcdc54866bb8023c0972df8e
JoelKM <joelkasmor16@gmail.com>
2023-05-02 00:51:12 +0200

    Re-order process

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import {Client, GatewayIntentBits} from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
     intents: [
@@ -13,14 +13,15 @@ const client = new Client({
     ]
 })
 
-client.login(process.env.DISCORD_TOKEN)
 
-console.log("DePerrito aktivo klk");
+client.once("ready", () => {
+    console.log(`Connected as ${client.user.tag}`);
+})
 
 client.on("messageCreate", async (message) => {
     const command = message.content.split(" ");
 
-    if(command[0]=='$pls') {
+    if (command[0] == '$pls') {
         switch (command[1]) {
             case 'help':
                 message.channel.send("///LISTA DE COMANDOS///");
@@ -33,3 +34,5 @@ client.on("messageCreate", async (message) => {
         }
     }
 })
+
+client.login(process.env.DISCORD_TOKEN)
